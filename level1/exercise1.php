71c0c8bbdb0a4dca486e798f5f1b9c63cac4b698
AlexPasquero <axelpasquero@gmail.com>
2025-03-23 12:43:44 +0100

    level 1 peer corrected

--- level1/exercise1.php
+++ level1/exercise1.php
@@ -3,11 +3,8 @@ declare(strict_types=1);
 
 $newArray=[2,3,5,7,11];
 
-echo $newArray[0] . PHP_EOL;
-echo $newArray[1] . PHP_EOL;
-echo $newArray[2] . PHP_EOL;
-echo $newArray[3] . PHP_EOL;
-echo $newArray[4] . PHP_EOL;
-
+foreach($newArray as $num){
+    echo $num . PHP_EOL;
+}
 
 ?>
