9f65bd3b0557dd3b0877bc1573133cee9edff814
AlexPasquero <axelpasquero@gmail.com>
2025-03-23 11:38:05 +0100

    level 1 exercises

---
+++ level1/exercise1.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+$newArray=[2,3,5,7,11];
+
+echo $newArray[0] . PHP_EOL;
+echo $newArray[1] . PHP_EOL;
+echo $newArray[2] . PHP_EOL;
+echo $newArray[3] . PHP_EOL;
+echo $newArray[4] . PHP_EOL;
+
+
+?>
